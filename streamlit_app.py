4beff0ba1bd387b40a55f3f8e60f6447e15c21b5
Jessica Sandagorda <105687452+jessicasandagorda@users.noreply.github.com>
2025-07-17 11:50:38 -0300

    Update streamlit_app.py
    cnx = st.connection("snowflake", type="snowflake")

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,18 +13,7 @@ from snowflake.snowpark.functions import col
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
 
-cnx = st.connection(
-    "snowflake",
-    type="snowflake",
-    user="jessicasandagorda",
-    password="35261464Color!",
-    account="xwb90052",
-    warehouse="COMPUTE_WH",
-    database="SMOOTHIES",
-    schema="PUBLIC",
-    role="SYSADMIN"
-)
-st.write("Conexión OK:", cnx)
+cnx = st.connection("snowflake", type="snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
