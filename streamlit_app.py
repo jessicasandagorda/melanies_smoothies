cd331ba20b2da20608ece40ee10bd43488edea9a
Jessica Sandagorda <105687452+jessicasandagorda@users.noreply.github.com>
2025-07-17 15:38:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,9 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
     
-    time_to_insert = st.button('Submit Order')
+time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+   if time_to_insert:
+      session.sql(my_insert_stmt).collect()
 
-        st.success('Your Smoothie is ordered'+ "," + name_on_order+'!', icon='✅')
+      st.success('Your Smoothie is ordered'+ "," + name_on_order+'!', icon='✅')
